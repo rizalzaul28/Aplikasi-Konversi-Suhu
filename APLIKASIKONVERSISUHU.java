5ec6aa2e8bda881a39457bac442ce5bebc092443
rizalzaul28 <163430703+rizalzaul28@users.noreply.github.com>
2024-05-29 22:15:36 +0700

    Add files via upload

---
+++ APLIKASIKONVERSISUHU.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi.konversi.suhu;
+
+/**
+ *
+ * @author user
+ */
+public class APLIKASIKONVERSISUHU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
